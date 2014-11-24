2ab233ae2b48cbd7ea317565013498da57a8038f
sparkhead95 <christianosholland@gmail.com>
2014-11-24 22:14:34 +0000

    attempted bubble sort. Testing started

---
+++ CompetitionManager/src/uk/ac/aber/dcs/chh57/cs21120/assignment1/BubbleEliminationTest.java
@@ -0,0 +1,34 @@
+package uk.ac.aber.dcs.chh57.cs21120.assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BubbleEliminationTest {
+
+	@Test
+	public void testSetPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasNextMatch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextMatch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMatchWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+}
